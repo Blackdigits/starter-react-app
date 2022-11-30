5242a41753532b5b2e5ba7f8803e2871837ffa4f
Blackdigits <galaxian125@gmail.com>
2022-11-30 12:40:30 +0700

    asg

---
+++ src/pages/AdminCreateBanner/index.jsx
@@ -0,0 +1,150 @@
+import React, {useState, useEffect} from 'react';
+import styled from "styled-components";
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import swal from 'sweetalert';
+import { Typography, TextField, Alert, Button } from '@mui/material';
+import { createNewBanner } from '../../features/banner/bannerSlice';
+
+const Container = styled.div`
+    display: flex;
+    flex: 4;
+    flex-direction: column;
+    padding: 20px;
+    border: 1px solid rgba(0,0,0,.125);
+    margin: 0 10px 10px 10px;
+    border-radius: 5px;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    width: 30%;
+    padding: 20px;
+`;
+
+const Column = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+`
+
+const InputContainer = styled.div`
+    margin: 10px;
+    input[type=number]::-webkit-inner-spin-button, 
+    input[type=number]::-webkit-outer-spin-button { 
+    -webkit-appearance: none; 
+    margin: 0; 
+    }
+`;
+
+const AlertContainer = styled.div`
+    margin-top: 20px
+`;
+
+const ImageRow = styled.div`
+    display: flex;
+    margin: 10px;
+`;
+
+const Image = styled.img`
+    width: 150px;
+    height: 150px;
+`;
+
+const InputFile = styled.input`
+`;
+
+const AdminCreateBanner = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+    const [image, setImage] = useState('');
+    const [imagePreview, setImagePreview] = useState();
+    const dataBanner = useSelector(state => state.banner); 
+    const [errorInput, setErrorInput] = useState(false);
+
+    const handleImage = (e) => {
+        const file = e.target.files[0];
+        setImage(file);
+        setImagePreview(URL.createObjectURL(file));
+    }
+
+    const handleSubmit = () => {
+        if(title && desc && image){
+            setErrorInput(false);
+            const data = new FormData();
+            data.append('title', title.toLowerCase());
+            data.append('desc', desc.toLowerCase());
+            data.append('image', image)
+            setLoading(true);
+            dispatch(createNewBanner(data));
+        }
+        else {
+            setErrorInput(true);
+        }
+    };
+    useEffect(() => {
+        if(dataBanner.dataResponse){
+            swal("Success!", "Banner baru berhasil ditambahkan!", "success");
+            setLoading(false);
+            navigate('/admin/banner');
+        }
+    }, [dataBanner.isSuccess]);
+  return (
+    <Container>
+        <Typography variant="h5">TAMBAH DATA BANNER</Typography>
+        {errorInput &&
+        <AlertContainer>
+            <Alert severity="error">Semua field harus diisi!</Alert>
+        </AlertContainer>        
+        }
+        <Wrapper>
+            <Column>
+            <InputContainer>
+                <TextField
+                    label="Judul Banner"
+                    variant="filled"
+                    fullWidth
+                    size='normal'
+                    type='text'
+                    autoComplete="off"
+                    inputProps={{style: {fontSize: 13,}}}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+            </InputContainer>
+            <InputContainer>
+                <TextField
+                    label="Deskripsi Banner"
+                    variant="filled"
+                    fullWidth
+                    size='normal'
+                    type='text'
+                    multiline={true}
+                    minRows={4}
+                    autoComplete="off"
+                    inputProps={{style: {fontSize: 13}}}
+                    value={desc}
+                    onChange={(e) => setDesc(e.target.value)}
+                />
+            </InputContainer>
+            <ImageRow>
+            {imagePreview && <Image src={imagePreview} alt="images"/>}
+                <InputContainer>
+                    <InputFile type='file' onChange={handleImage} />
+                </InputContainer>
+            </ImageRow>
+            </Column>
+        </Wrapper>
+        {loading ? 
+        <Button variant="contained" disabled color="success" sx={{ width: '25%', marginLeft: '30px', marginTop: '20px' }}>Loading ....</Button>
+        :
+        <Button variant="contained" color="success" sx={{ width: '25%', marginLeft: '30px', marginTop: '20px' }} onClick={handleSubmit} >Tambah Banner Baru</Button>
+        }
+    </Container>
+  )
+}
+
+export default AdminCreateBanner;
---
+++ src/pages/AdminCreateProduct/index.jsx
@@ -0,0 +1,287 @@
+import React, {useEffect, useState} from 'react'
+import styled from "styled-components";
+import {createNewProduct} from "../../features/product/productSlice";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import swal from "sweetalert";
+import { Typography, TextField, Select, MenuItem, FormControl, InputLabel, Button, Alert } from '@mui/material';
+const Container = styled.div`
+    display: flex;
+    flex: 4;
+    flex-direction: column;
+    padding: 20px;
+    border: 1px solid rgba(0,0,0,.125);
+    margin: 0 10px 10px 10px;
+    border-radius: 5px;
+`;
+
+const AlertContainer = styled.div`
+    margin-top: 20px
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    padding: 20px;
+`;
+
+const LeftColumn = styled.div`
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+`
+const RightColumn = styled.div`
+    display: flex;
+    flex: 2;
+    flex-direction: column;
+`
+
+const InputContainer = styled.div`
+    margin: 10px;
+    input[type=number]::-webkit-inner-spin-button, 
+    input[type=number]::-webkit-outer-spin-button { 
+    -webkit-appearance: none; 
+    margin: 0; 
+    }
+`;
+
+const SelectContainer = styled.div`
+    display: flex;
+    margin: 10px;
+`;
+
+const ImageContainer = styled.div`
+    margin: 10px;
+`;
+
+const ImageRow = styled.div`
+    display: flex;
+    margin-top: 10px;
+`;
+
+const Image = styled.img`
+    width: 75px;
+    height: 75px;
+`;
+
+const InputFile = styled.input`
+    margin-top: 20px;
+`;
+ 
+const AdminCreateProduct = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const [loading, setLoading] = useState(false);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [promoPrice, setPromoPrice] = useState(0);
+    const [desc, setDesc] = useState('');
+    const [category, setCategory] = useState('');
+    const [type, setType] = useState('');
+    const [colors, setColors] = useState([{id: 1, color: '', image: '', imagePreview: ''}]);
+    const categories = useSelector(state => state.category); 
+    const dataProduct = useSelector(state => state.product); 
+    const types = ['best seller', 'new', 'normal', 'sale'];
+    const [errorInput, setErrorInput] = useState(false);
+    const handleColor = (id, e) => {
+        setColors(colors.map(item => {
+            if(item.id === id){
+                return {...item, color: e.target.value.toLowerCase()}
+            } else {
+                return item
+            }
+        }))
+    }
+    const handleImage = (id, e) => {
+        const file = e.target.files[0];
+        setColors(colors.map(item => {
+            if(item.id === id){
+                return {...item, image: file, imagePreview: URL.createObjectURL(file)}
+            } else {
+                return item;
+            }
+        }))
+    }
+    const addImageButton = () => {
+        const id = colors.length + 1
+        const color = ''
+        const image = ''
+        const imagePreview = ''
+        setColors([...colors, {id, color, image, imagePreview}]);
+    }
+    const deleteImageButton = (id) => {
+        setColors(colors.filter(item => item.id !== id));
+    }
+    const handleSubmit = () => {
+        if(name && price && desc && category && type) {
+            colors.map(color => {
+                if(color.color && color.image){
+                    setErrorInput(false)
+                } else {
+                    setErrorInput(true);
+                }
+            })
+            if(errorInput !== true){
+                const data = new FormData();
+                data.append('name', name.toLowerCase());
+                data.append('desc', desc.toLowerCase());
+                data.append('price', price);
+                data.append('promoPrice', promoPrice);
+                data.append('colors', JSON.stringify(colors))
+                colors.map((item) => {
+                    data.append('image', item.image)
+                });
+                data.append('category', category);
+                data.append('type', type);
+                setLoading(true);
+                dispatch(createNewProduct(data));
+            }
+        }
+        else {
+            setErrorInput(true);
+        }
+    }
+    useEffect(() => {
+        if(dataProduct.data){
+            swal("Success!", "Produk baru berhasil ditambahkan!", "success");
+            setLoading(false);
+            navigate('/admin/produk');
+        }
+    }, [dataProduct.isSuccess])
+  return (
+    <Container>
+        <Typography variant="h5">TAMBAH DATA PRODUK</Typography>
+        {errorInput &&
+        <AlertContainer>
+            <Alert severity="error">Semua field harus diisi!</Alert>
+        </AlertContainer>        
+        }
+        <Wrapper>
+            <LeftColumn>
+            <InputContainer>
+                <TextField
+                    label="Nama Produk"
+                    variant="filled"
+                    fullWidth
+                    size='normal'
+                    type='text'
+                    autoComplete="off"
+                    inputProps={{style: {fontSize: 13,}}}
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </InputContainer>
+            <InputContainer>
+                <TextField
+                    label="Harga Normal"
+                    variant="filled"
+                    fullWidth
+                    size='normal'
+                    type='number'
+                    autoComplete="off"
+                    inputProps={{style: {fontSize: 13}}}
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                />
+            </InputContainer>
+            <InputContainer>
+                <TextField
+                    label="Harga Promo"
+                    variant="filled"
+                    fullWidth
+                    size='normal'
+                    type='number'
+                    autoComplete="off"
+                    inputProps={{style: {fontSize: 13}}}
+                    value={promoPrice}
+                    onChange={(e) => setPromoPrice(e.target.value)}
+                />
+            </InputContainer>
+            <InputContainer>
+                <TextField
+                    label="Deskripsi Produk"
+                    variant="filled"
+                    fullWidth
+                    size='normal'
+                    type='text'
+                    multiline={true}
+                    minRows={4}
+                    autoComplete="off"
+                    inputProps={{style: {fontSize: 13}}}
+                    value={desc}
+                    onChange={(e) => setDesc(e.target.value)}
+                />
+            </InputContainer>
+            </LeftColumn>
+            <RightColumn>
+                <SelectContainer>
+                    <FormControl variant="filled" fullWidth size="normal" sx={{ marginRight: 2 }}>
+                        <InputLabel id="select-category">Kategori</InputLabel>
+                        <Select
+                            labelId="select-category"
+                            value={category}
+                            label="Age"
+                            onChange={(e) => setCategory(e.target.value.toLowerCase())}
+                        >
+                            {categories.isLoading ?
+                                <div>sedang loading</div>
+                                :
+                                categories.data ?
+
+                                categories.data.data.map((item) => (<MenuItem key={item._id} value={item.name}>{item.name.toUpperCase()}</MenuItem>))
+                                :
+                                ''
+                            }
+                        </Select>
+                    </FormControl>
+                    <FormControl variant="filled" fullWidth>
+                        <InputLabel id="select-type">Type</InputLabel>
+                        <Select
+                            labelId="select-type"
+                            value={type}
+                            label="Age"
+                            onChange={(e) => setType(e.target.value.toLowerCase())}
+                        >
+                            {types.map((item, index) =>(<MenuItem key={index} value={item}>{item.toUpperCase()}</MenuItem>))}
+                        </Select>
+                    </FormControl>
+                </SelectContainer>
+                <ImageContainer>
+                {
+                colors.map((item) => 
+                    (<ImageRow key={item.id}>
+                        {item.imagePreview && <Image src={item.imagePreview} alt="images"/>}
+                        <InputContainer>
+                            <TextField
+                                label="Warna"
+                                variant="filled"
+                                fullWidth
+                                size='normal'
+                                type='text'
+                                autoComplete="off"
+                                inputProps={{style: {fontSize: 13,}}}
+                                value={item.color}
+                                onChange={(e) => handleColor(item.id, e)}
+                            />
+                        </InputContainer>
+                        <InputContainer>
+                            <InputFile type='file' onChange={(e) => handleImage(item.id, e)} />
+                        </InputContainer>
+                        {item.id > 1 ?
+                            <Button variant="contained" size="small" sx={{height: '30px', marginLeft: '5px', marginTop: '25px', backgroundColor: 'red' }} onClick={ () => deleteImageButton(item.id)} >Hapus</Button> : ''}
+                    </ImageRow>)
+                )
+                }
+                </ImageContainer>
+                <Button variant="contained" sx={{ width: '25%', marginLeft: '10px', marginTop: '10px' }} onClick={addImageButton} >Tambah Warna</Button>
+            </RightColumn>
+        </Wrapper>
+        {loading ? 
+        <Button variant="contained" disabled color="success" sx={{ width: '25%', marginLeft: '30px', marginTop: '20px' }}>Loading ....</Button>
+        :
+        <Button variant="contained" color="success" sx={{ width: '25%', marginLeft: '30px', marginTop: '20px' }} onClick={handleSubmit} >Tambah Produk Baru</Button>
+        }
+    </Container>
+  )
+}
+
+export default AdminCreateProduct;
