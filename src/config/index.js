2e52d5c3b4ea58fdd020315c31b1f98c78388acf
Blackdigits <69674187+Blackdigits@users.noreply.github.com>
2022-12-11 19:11:08 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const host_url = "http://localhost:4000";
-export const host_url_api = "http://localhost:4000/api/v1/";
+export const host_url = "https://busy-underwear-clam.cyclic.app/";
+export const host_url_api = "https://busy-underwear-clam.cyclic.app/api/v1/";
