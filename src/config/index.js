31b669b53c1b50cebca4c69bc71110ddbe6d7416
Muhammad Reza <70399492+muhammadrezaa21@users.noreply.github.com>
2022-12-11 20:59:37 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const host_url = "balancia.co.id:4000";
-export const host_url_api = "balancia.co.id:4000/api/v1/";
+export const host_url = "http://balancia.co.id:4000/";
+export const host_url_api = "http://balancia.co.id:4000/api/v1/";
