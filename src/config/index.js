e34deede7ab7c6cafc9738a6a69f6abb51d9fa5f
Blackdigits <69674187+Blackdigits@users.noreply.github.com>
2022-12-14 10:56:01 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,2 +1,2 @@
-export const host_url = "http://balancia.co.id:4000/";
-export const host_url_api = "http://balancia.co.id:4000/api/v1/";
+export const host_url = "https://balancia.co.id:4000/";
+export const host_url_api = "https://balancia.co.id:4000/api/v1/";
